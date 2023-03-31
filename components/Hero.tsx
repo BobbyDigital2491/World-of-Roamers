86f6c91e8a29e7b497b4cba2cbb7289b37300ae5
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2023-03-31 11:29:17 -0400

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,7 +10,6 @@ const Hero = () => {
         <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none text-yellow-400 md:text-5xl xl:text-6xl dark:text-yellow-400">Pieces 2 Piece</h1>
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white dark:text-white">Wellness Center LLC</h1>
-            <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.</p>
             <a href="/about" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base bg-yellow-400 font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-white hover:text-yellow-400 focus:ring-4 focus:ring-primary-300 active:bg-yellow-400 active:text-white dark:focus:ring-primary-900">
                 Learn More
                 <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
