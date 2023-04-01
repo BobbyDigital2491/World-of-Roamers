7a35faeb4ea6a9ef351bf7167d0edee0f15c55d9
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2023-04-01 19:12:29 -0400

    Update Services.tsx

--- components/Services.tsx
+++ components/Services.tsx
@@ -7,7 +7,7 @@ const Services = () => {
         <a href="#" className="flex flex-col items-center bg-black border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
             <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="https://sdhcounseling.com/wp-content/uploads/2014/09/AdobeStock_38376118.jpeg" alt=""/>
             <div className="flex flex-col justify-between p-4 leading-normal">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-yellow-400 dark:text-yellow-400">Pre-martial Counselling</h5>
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-yellow-400 dark:text-yellow-400">Pre-marital Counselling</h5>
                 <p className="mb-3 font-normal text-white dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
             </div>
         </a>
