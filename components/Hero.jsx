080fc26efc9732ebf43555036d165226ea9789c5
Robbob24 <r.lawrence2491@gmail.com>
2023-12-06 13:23:18 -0500

    19th

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 const Hero = ({heading, message}) => {
   return (
-    <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover bg-transparent'>
+    <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover'>
   <video autoPlay loop muted playsInline className='absolute top-0 left-0 w-full h-full object-cover z-0'>
-  <source src='https://www.facebook.com/100000191167297/videos/282028248159168/' type='video/mp4' />
+  <source src="https://www.dropboxusercontent.com/s/jrfotyfs0d8sqmysi4xdj/download.mp4" type='video/mp4' />
 </video>
       <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/20 z-[2]' />
       <div className='p-5 text-white z-[2] mt-[-10rem]'>
