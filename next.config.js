07ed176078e754b5ab5e78d195a0218924842a82
Robbob24 <r.lawrence2491@gmail.com>
2023-12-06 00:51:13 -0500

    9th

--- next.config.js
+++ next.config.js
@@ -6,8 +6,10 @@ const nextConfig = {
 module.exports = {
   images: {
     domains: [
-      'images.unsplash.com'
-      ,
+      'images.unsplash.com',
+      'www.google.com', // Example Google domain
+      'https://www.dropbox.com/scl/fi/jrfotyfs0d8sqmysi4xdj/download.mp4?rlkey=86ibm0ft2as7z6jivvo68s2iw&dl=0', // Example Dropbox domain
+      // Add other domains here as needed
     ]
   }
 }
