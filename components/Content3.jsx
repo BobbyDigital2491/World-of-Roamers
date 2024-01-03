c889baebbea2b524781873316aaac81275509a06
Robbob24 <r.lawrence2491@gmail.com>
2024-01-03 18:00:50 -0500

    41st commit

--- components/Content3.jsx
+++ components/Content3.jsx
@@ -12,17 +12,43 @@ const Content3 = () => {
             Revolutionize training and education <br className="hidden lg:block"/>
             through virtual reality.
           </h1>
-          <p className="max-w-xl mx-auto mt-8 text-base leading-relaxed text-gray-500">World of Roamers is a revolutionary way to explore various types of immersive experiences. Whether you&apos;re a seasoned vet or a beginner in the virtual reality space, everyone will roam freely in their own respective universe. We will be the solution to so many problems that take place in this reality all while helping our community explore new and innovative possibilities. The technological advances in our world are steadily evolving and it is our responsibility to make sure you are ready for it.</p>
+          <p className="max-w-xl mx-auto mt-8 text-base leading-relaxed text-gray-500">World of Roamers is a revolutionary way to explore various types of immersive experiences. Whether you&apos;re a seasoned vet or a beginner in the virtual reality space, everyone will roam freely in their own respective universe. We are the solution to many problems that take place in our real reality including support for immobilized individuals, alleviating social isolation, cognitive stimulation, and emotional well being.  All while helping our community explore new and innovative possibilities. The technological advances in our world are steadily evolving and it is our responsibility to make sure you are able to enjoy it!</p>
           
          
         </div>
       </div>
     </div>
-    <section id="intro">
-      <div className="flex flex-col items-center justify-center pt-24 mx-auto rounded-lg lg:px-10 max-w-7xl">
-        <img className="object-cover object-center w-full rounded-xl" alt="hero" src="/tech.gif"/>
-      </div>
-    </section>
+    <section>
+        
+    <div className="relative items-center w-full px-5 py-12 mx-auto md:px-12 lg:px-24 max-w-7xl">
+        <div className="grid w-full grid-cols-1 gap-6 mx-auto lg:grid-cols-3">
+            <div className="p-6">
+                <img className="object-cover object-center w-full mb-8 lg:h-48 md:h-36 rounded-xl" src="vr4.png" alt="blog"/>
+                <div className="inline-flex justify-between w-full">
+                    <a href='/game1'><h1 className="mb-8 text-xl font-semibold leading-none tracking-tighter text-transparent bg-clip-text bg-gradient-to-r from-indigo-700 to-pink-600">Senior Citizen World Exploration</h1></a>
+                    
+                </div>
+                <p className="mx-auto text-base font-medium leading-relaxed text-gray-500">We bring the world to seniors, offering virtual exploration, engaging activities, and vibrant community connections. Step into a realm of boundless possibilities, where technology creates joy, connection, and a sense of adventure for our cherished seniors.</p>
+            </div>
+            <div className="p-6">
+                <img className="object-cover object-center w-full mb-8 lg:h-48 md:h-36 rounded-xl" src="https://cdn.cloudflare.steamstatic.com/steam/apps/787790/ss_d54386550b3db5180e87d6e655a38286f646c9df.1920x1080.jpg?t=1697721825" alt="blog"/>
+                <div className="inline-flex justify-between w-full">
+                    <h1 className="mb-8 text-xl font-semibold leading-none tracking-tighter text-transparent bg-clip-text bg-gradient-to-r from-indigo-700 to-pink-600">Birthday Parties for all ages</h1>
+                    
+                </div>
+                <p className="mx-auto text-base font-medium leading-relaxed text-gray-500">The ultimate mobile virtual reality party experience! Immerse yourself and your guests in a world of interactive fun, adventure, and laughter, bringing a unique and unforgettable twist to birthdays and special occasions.</p>
+            </div>
+            <div className="p-6">
+                <img className="object-cover object-center w-full mb-8 lg:h-48 md:h-36 rounded-xl" src="vr3.png" alt="blog"/>
+                <div className="inline-flex justify-between w-full">
+                    <h1 className="mb-8 text-xl font-semibold leading-none tracking-tighter text-transparent bg-clip-text bg-gradient-to-r from-indigo-700 to-pink-600">Corporate Trainings</h1>
+                    
+                </div>
+                <p className="mx-auto text-base font-medium leading-relaxed text-gray-500">A cutting-edge solution that transcends traditional corporate training methods. Immerse your team in realistic scenarios, interactive simulations, and personalized learning modules, engagement and retention for a more impactful and efficient training experience.</p>
+            </div>
+        </div>
+    </div>
+</section>
   </div>
 </section>                            
   )
