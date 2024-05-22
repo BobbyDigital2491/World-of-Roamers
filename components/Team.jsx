9569b5a19e8e798bf7c39defc069141ee2a4c7e6
Robbob24 <r.lawrence2491@gmail.com>
2024-05-21 22:57:53 -0400

    51st

--- components/Team.jsx
+++ components/Team.jsx
@@ -9,7 +9,7 @@ const Team = () => {
             <h2 className="max-w-5xl text-2xl font-bold leading-none text-center tracking-tighter text-transparent bg-clip-text bg-gradient-to-r from-indigo-700 to-pink-600 md:text-5xl lg:text-6xl lg:max-w-7xl">Co - Founders</h2>
         </div>
 
-        <div className="grid max-w-6xl grid-cols-1 px-20 mx-auto mt-12 text-center sm:px-0 sm:grid-cols-2 md:mt-20 gap-x-8 md:grid-cols-4 gap-y-12 lg:gap-x-16 xl:gap-x-20">
+        <div className="grid max-w-6xl grid-cols-1 px-20 mx-auto mt-12 text-center sm:px-0 sm:grid-cols-1 md:mt-20 gap-x-8 md:grid-cols-3 gap-y-12 lg:gap-x-16 xl:gap-x-20">
         
             <div>
                 <img className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44" src="EJ.png" alt="" />
@@ -23,11 +23,7 @@ const Team = () => {
                 <p className="mt-2 text-base font-normal text-gray-600 font-pj">Chief Operating Officer</p>
             </div>
 
-            <div>
-                <img className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44" src="TJ.png" alt="" />
-                <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">Tawny Jones</p>
-                <p className="mt-2 text-base font-normal text-gray-600 font-pj">Chief Marketing Officer</p>
-            </div>
+           
 
             <div>
                 <img className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44" src="Rpb.jpg" alt="" />
